43173c5d6cb473a3dbd584fe52cf00f08d70533a
Mr.Char[/0/ ‘path’] <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-20 10:50:50 -0600

    Create m_bCaulk.cpp

---
+++ shader/m_bCaulk.cpp
@@ -0,0 +1,9 @@
+#include "m_bCaulk.h"
+
+const int m_bCaulkNumber = 0
+bool m_bCaulkLoaded(m_bCaulk& caulk);
+
+const char * CaulkGlobals(m_bCaulk * caulk){
+   static char m_bCBuff[caulk->m_bCaulk.c];
+   caulk->m_bCaulkId == m_bCaulkNumber++;
+};
