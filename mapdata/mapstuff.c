4cbcdc759ea3f4e3a69dfc3f1b6c695db803ae70
Mr.Char[/0/ ‘path’] <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-20 11:24:52 -0600

    Update mapstuff.c

--- mapdata/mapstuff.c
+++ mapdata/mapstuff.c
@@ -1,1 +1,13 @@
+#include "contrib/gamepath.h"
 
+char pPathC[0];// pPathC = path->cpath;
+const char * pMapName[0];
+
+const char * PathName( g_bGamePath * path ){
+ static char mapBuffer[1024];
+   path->cpath == pMapName('/n');
+   printf("%c", pPathC >= path->cpath);
+   sprintf(mapBuffer, "g_bGamePath %c", path);
+
+  return mapBuffer;
+};
