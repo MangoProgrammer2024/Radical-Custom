b0435c3c5064af4c34abf3738362b912fd7895b8
Mr.Char[/0/ ‘path’] <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-20 11:04:55 -0600

    Update m_bCaulk.cpp

--- shader/m_bCaulk.cpp
+++ shader/m_bCaulk.cpp
@@ -4,8 +4,8 @@ const int m_bCaulkNumber = 0
 bool m_bCaulkLoaded(m_bCaulk& caulk);
 
 const char * CaulkGlobals(m_bCaulk * caulk){
-   static char m_bCBuff[caulk->m_bCaulk.c];
-   caulk->m_bCaulkId == m_bCaulkNumber++;
+ static char m_bCBuff[caulk->m_bCaulk.c];
+ caulk->m_bCaulkId == m_bCaulkNumber++;
     if(caulk->m_bCaulk.m_bMode){
       m_bSysprintfCaulk(caulk, "m_bCaulk %c", caulk->m_bCaulk.m_bCaulkName(m_bCaulk));
       sprintf(m_bCBuff, "m_bCaulk", m_bCaulkNumberId);
