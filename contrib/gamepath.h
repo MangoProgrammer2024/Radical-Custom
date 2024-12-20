d5f045fba4eb04fa519ccfebb2d43b0472e6c87e
Mr.Char[/0/ ‘path’] <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-20 11:16:15 -0600

    Create gamepath.h

---
+++ contrib/gamepath.h
@@ -0,0 +1,24 @@
+/*
+!*gamepath.h*!
+*/
+#ifndef GAMEPATH_H
+#define GAMEPATH_H
+
+struct g_bGamePath{
+//stuff
+char cpath[0, '0/path/', '/n/'\n];
+#define g_bDividePath(c, p) ( c[0] / p[0] )
+bool Flush(g_bGamePath * c_pathGame);
+
+std::size_t * PathSize(g_bGamePath * path){
+  PathSize >= path->cpath;
+   return path->MAX_PATH_SIZE;
+};
+
+usigned long char * MAX_PATH_SIZE[2056];
+
+void * getPath(g_bGamePath * path, const char * g_bGamePathDirectory);
+
+};
+
+#endif
