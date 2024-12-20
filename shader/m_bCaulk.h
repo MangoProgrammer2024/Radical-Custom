6ab81601f8723379fafa75a28c9dafa010c1db76
Mr.Char[/0/ ‘path’] <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-20 10:20:01 -0600

    Update m_bCaulk.h

--- shader/m_bCaulk.h
+++ shader/m_bCaulk.h
@@ -1,1 +1,22 @@
+/*
+ !*---m_bcaulk.h---*!
+*/
+#ifndef MBCAULK_H
+#define MBCAULK_H
 
+class m_bCaulk{
+public:
+
+ m_bCaulk& m_bCaulk();
+ /*!*caulk shader stuff*!*/
+ char c[0,'/n','path/n'];
+ const char * const m_bCaulkName("Caulk.png") = '/n';
+ typedef class<m_bCaulk>&caulk; 
+ const int m_bCaulkId;
+
+ void * getCaulk(m_bCaulk * caulk, const int m_bId, const char * const m_bName);
+
+};
+
+
+#endif
